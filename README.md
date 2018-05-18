09cc656ef804cef16cc2031ae8b9836520b286c1
geek182 <geek182@gmail.com>
2018-05-18 17:16:32 -0300

    more tests

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # inspec
 i used this link to begin :) 
 http://www.anniehedgie.com/inspec-basics-1
+
+RUN on docker
+sudo inspec exec hello_spec.rb -t docker://a2b831014081
+
--- hello_spec.rb
+++ hello_spec.rb
@@ -5,4 +5,8 @@ control "world-1.0" do                                # A unique ID for this con
   describe file('hello.txt') do                       # The actual test
    its('content') { should match 'Hello World' }      # You could just do the "describe file" block if you want. The  
   end                                                 # rest is just metadata, but it's a good habit to get into.
+  describe file('/tmp') do
+  it { should be_directory }
+end
+
 end
